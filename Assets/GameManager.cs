a249eadc0384972e854d8b538c9555cc29a95542
frankvHoof93 <frank_van_hoof@hotmail.com>
2017-04-21 15:19:29 +0200

    Merge conflict zooi

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -55,13 +55,6 @@ public class GameManager : MonoBehaviour {
     /// <returns></returns>
     public Player GetPlayer(int playerID)
     {
-<<<<<<< HEAD
-=======
-        foreach (int k in players.Keys)
-        {
-            Debug.Log("Devid " + k);
-        }
->>>>>>> origin/master
         return players[playerID];
     }
 
